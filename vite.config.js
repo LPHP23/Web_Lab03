766a4f91be0d87171eadf369b6517c363e30aea5
Thanh Thu <165758072+LPHP23@users.noreply.github.com>
2025-11-11 10:29:46 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ Cực kỳ quan trọng: tên repo chính xác, có dấu “/” 2 bên
   base: '/Web_Lab03/',
 })
